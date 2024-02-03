1156060d027f22f09262be36ecdce87a51e8b427
EssiJunior <nessipjunior@gmail.com>
2024-02-03 14:39:03 +0100

    Done task tests... Terminated tests!

--- index.test.js
+++ index.test.js
@@ -17,19 +17,21 @@ describe('GET /', function() {
 });
 
 
+// # ------------------------------ Tests for user APIs --------------------------------------
+
 /**
  * This function makes a POST request to the signin route of the Express application and 
  * verifies that the response status code is 200.
  */
 describe('POST /api/signin', function() {
-    it('responds with 200 status code', async function() {
+    it('responds with 404 status code', async function() {
         const response = await request(application)
             .post('/api/signin')
             .send({ 
-                email: 'essi@junior.com', 
-                password: '1234' 
+                email: "user@email.com", 
+                password: 'string' 
             })
-        expect(response.status).toBe(200);
+        expect(response.status).toBe(404);
         server.close();
     });
 });
@@ -39,16 +41,67 @@ describe('POST /api/signin', function() {
  * verifies that the response status code is 400 because the specified email is already used.
  */
 describe('POST /api/signup', function() {
-    it('responds with 400 status code', async function() {
+    it('responds with validation errer', async function() {
         const response = await request(application)
             .post('/api/signup')
             .send({
-                firstName: "string",
-                lastName: "string",
-                email: "essi@junior.com",
-                password: "string"
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: ""
             })
-        expect(response.status).toBe(400);
+            expect(response.type).toMatch(/json/);
+            expect(response.body).toEqual({ error: "Validation error" });
         server.close();
     });
 });
+// # ------------------------------ ------------------- --------------------------------------
+
+// # ------------------------------ Tests for task APIs --------------------------------------
+
+describe('POST /api/task', function() {
+    it('responds with 201 status code', async function() {
+        const response = await request(application)
+            .post('/api/task')
+            .send({
+                title: "Title of a task"
+            })
+        expect(response.status).toBe(201);
+        server.close();
+    });
+});
+
+describe('PATCH /api/task/title/:id', function() {
+    it('responds with 404 status code', async function() {
+        const response = await request(application)
+            .patch('/api/task/title/1000000')
+            .send({
+                title: "New title of a task"
+            })
+        expect(response.status).toBe(404);
+        server.close();
+    });
+});
+
+describe('PATCH /api/task/status/:id', function() {
+    it('responds with 404 status code', async function() {
+        const response = await request(application)
+            .patch('/api/task/status/1000000')
+            .send({
+                status: "done"
+            })
+        expect(response.status).toBe(404);
+        server.close();
+    });
+});
+
+
+describe('DELETE /api/task/:id', () => {
+    it('responds with 404 status code', async function() {
+        const response = await request(application)
+            .delete('/api/task/1000000')
+        expect(response.status).toBe(404);
+        server.close();
+    });
+});
+// # ------------------------------ ------------------- --------------------------------------
